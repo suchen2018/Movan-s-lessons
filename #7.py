59a4131fefbd562df15a60597f136f061c4693cd
suchen2018 <38933025+suchen2018@users.noreply.github.com>
2018-07-13 23:50:25 +1000

    Checkbutton 勾选项

---
+++ #7.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 21:49:48 2018
+
+@author: Chen
+"""
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('400x400')
+
+l = tk.Label(window, bg='yellow', width=20, text='empty')
+l.pack()
+
+def print_selection():
+    if (var1.get()==1) & (var2.get()==0):
+        l.config(text='I love Python')
+    elif (var1.get()==0) & (var2.get()==1):
+        l.config(text='I love C++')
+    elif (var1.get()==1) & (var2.get()==1):
+        l.config(text='I love both')
+    else:
+        l.config(text="I don't love either")
+    
+    
+
+var1 = tk.IntVar()
+var2 = tk.IntVar()
+c1 = tk.Checkbutton(window, text='Python', variable=var1, 
+                    onvalue=1, offvalue=0, command=print_selection)
+c2 = tk.Checkbutton(window, text='C++', variable=var2, 
+                    onvalue=1, offvalue=0, command=print_selection )
+
+c1.pack()
+c2.pack()
+
+window.mainloop()
